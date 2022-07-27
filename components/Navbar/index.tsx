8fa70bd3ed013f77a3287627567035472cd6595c
Alejo <alejoivanrojas@gmail.com>
2022-07-26 21:13:52 -0300

    styles added

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav style={{width: "90vw", padding: "1rem", display: "flex", justifyContent: "space-between"}} >
+    <nav>
         <Image src="/logo.png" alt="logo-netflix" width={150} height={55} />
-        <a style={{left: "0"}}>Iniciar sesion</a>
+        <a>Iniciar sesion</a>
     </nav>
   )
 }
